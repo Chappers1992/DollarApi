6f13373f93766401b15d43e7a7e4a014688041a3
Chappers1992 <57559262+Chappers1992@users.noreply.github.com>
2020-09-23 12:25:16 +0100

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,19 +1,18 @@
 from flask import request, redirect
 from flask_restx import Resource
 
-from api.uprocs.function import NewTask
-from api.uprocs.dto import ns, new_task_post_dto
+from api.tasks.function import NewTask
+from api.tasks.dto import ns, new_task_post_dto
 
 
-@ns.route('/RunUprocs')
+@ns.route('/RunTask')
 class RunTask(Resource):
     @staticmethod
-    @ns.expect(new_task_post_dto, required=True)
+    @ns.expect(new_task_post_dto, validate=True)
     def post():
         request_data = request.get_json()
-        command_line = NewTask.generate_commandline(request_data['task'], request_data['mu'], request_data['processing_date'], request_data['start_date'],
+        command_line = NewTask.run_commandline(request_data['task'], request_data['mu'], request_data['processing_date'], request_data['start_date'],
                                                     request_data['args'])
         if command_line:
             return command_line
-        # return request_data
         return None, 400
